eaf42d6617ac95d1bbe0378432b393ba3d3e44f7
An Dang <an.dang_thanh@mailbox.tu-dresden.de>
2022-08-09 11:49:36 +0200

    added ClassSpecificImageFolder

--- code/splittedCode/databunch.py
+++ code/splittedCode/databunch.py
@@ -1,3 +1,8 @@
+
+from torchvision import datasets
+IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
+
+
 class DataBunch():
 
     def __init__(self, train_dl, valid_dl, c=None):
@@ -12,4 +17,32 @@ class DataBunch():
     @property
     def valid_ds(self):
         return self.valid.dl.dataset
+
+
+class ClassSpecificImageFolder(datasets.DatasetFolder):
+    def __init__(
+            self,
+            root,
+            dropped_classes=[],
+            transform = None,
+            target_transform = None,
+            loader = datasets.folder.default_loader,
+            is_valid_file = None,
+    ):
+        self.dropped_classes = dropped_classes
+        super(ClassSpecificImageFolder, self).__init__(root, loader, IMG_EXTENSIONS if is_valid_file is None else None,
+                                                       transform=transform,
+                                                       target_transform=target_transform,
+                                                       is_valid_file=is_valid_file)
+        self.imgs = self.samples
+
+    def find_classes(self, directory):
+        classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
+        classes = [c for c in classes if c not in self.dropped_classes]
+        if not classes:
+            raise FileNotFoundError(f"Couldn't find any class folder in {directory}.")
+
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
+
 
