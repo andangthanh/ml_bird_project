7058c7863c52e8eb4ef9a33c8954f3f79f8e21c6
An Dang Thanh <andangthanh@gmail.com>
2022-08-31 08:24:27 +0200

    fixed missing : after if statement

--- code/splittedCode/learner.py
+++ code/splittedCode/learner.py
@@ -117,7 +117,7 @@ class Runner():
                 if not self('begin_epoch'): self.all_batches(self.databunch.train_dl)
 
                 # validation mode
-                if self.rank == 0 or self.rank == None
+                if self.rank == 0 or self.rank == None:
                     with torch.no_grad():
                         if not self('begin_validate'): self.all_batches(self.databunch.valid_dl)
                 self.epoch += 1
